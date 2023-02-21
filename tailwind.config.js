9d1fe7341cdc9018e91d9d599b7169ff2b1427d8
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-02-21 23:48:09 +0530

    chnages for netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   /* content: ["./src/Components/*.{html,css, js, jsx}"], */
-  content: ["./src/components/**/*.jsx"],
+  content: ["./src/components/**/*.jsx", "./public/index.html"],
   theme: {
     extend: {},
   },
