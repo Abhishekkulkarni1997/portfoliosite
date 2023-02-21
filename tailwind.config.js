e816528aca22880f48c0053092911925b14a75aa
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-02-22 00:02:44 +0530

    changes for tailwind netlify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const { join } = require("path");
 module.exports = {
-  content: ["./src/components/**/*.jsx", "./public/index.html"],
   content: [join(__dirname, "./src/**/*.jsx")],
   theme: {
     extend: {},
